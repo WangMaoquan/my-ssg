4ae68ee16d52289be8017e4440146c728fa14611
wangmaoquan <1782976211@qq.com>
2023-05-22 23:38:03 +0800

    feat: 新增服务端调用组件的代码

---
+++ src/runtime/ssr-entry.tsx
@@ -0,0 +1,12 @@
+import { App } from './App';
+import { renderToString } from 'react-dom/server';
+
+/**
+ * SSG 本质是构建阶段的 SSR, 而当下的 SSR 会采用同构架构, 也就是同样的组件 需要运行在服务端也要运行在客户端
+ *
+ * 下面就是 运行在 服务端的 App 代码
+ */
+
+export function render() {
+  return renderToString(<App />);
+}
