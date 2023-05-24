b81e123ed7f6296f2caa140e3aa214cb7ea788bc
wangmaoquan <1782976211@qq.com>
2023-05-24 20:47:46 +0800

    feat: vitest 测试环境搭建

---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitest/config';
+export default defineConfig({
+  test: {
+    environment: 'node', // 指定测试环境
+    exclude: ['**/node_modules/**', '**/dist/**'], // 排除文件夹
+    threads: true // 开启多线程
+  }
+});
