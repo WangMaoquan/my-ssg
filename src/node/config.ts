1648ad006e7d64aca1a7003f4786c1857301ee3d
wangmaoquan <1782976211@qq.com>
2023-05-25 15:21:17 +0800

    feat: 解析配置文件功能

---
+++ src/node/config.ts
@@ -0,0 +1,56 @@
+import fs from 'fs-extra';
+import { resolve } from 'path';
+import { loadConfigFromFile } from 'vite';
+import { UserConfig } from '../shared/types';
+
+const supportConfigFiles = ['config.ts', 'config.js'];
+
+type RawConfig =
+  | UserConfig
+  | Promise<UserConfig>
+  | (() => UserConfig | Promise<UserConfig>);
+
+function getUserConfigPath(root: string) {
+  try {
+    const configPath = supportConfigFiles
+      .map((file) => resolve(root, file))
+      .find(fs.pathExistsSync);
+    return configPath;
+  } catch (e) {
+    console.log('Failed to load user config');
+    throw e;
+  }
+}
+
+export async function resolveConfig(
+  root: string, // 项目根目录
+  command: 'serve' | 'build', // 指令
+  mode: 'development' | 'production' // 模式
+) {
+  // 1. 获取配置文件路径 支持 js/ ts
+  const configPath = getUserConfigPath(root);
+  // 2. 读取配置文件的内容 使用 vite 内置的方法
+  const result = await loadConfigFromFile(
+    {
+      command,
+      mode
+    },
+    configPath,
+    root
+  );
+
+  if (result) {
+    const { config: rawConfig = {} as RawConfig } = result;
+    // 1. object
+    // 2. promise
+    // 3. function
+
+    const userConfig = await (typeof rawConfig === 'function'
+      ? rawConfig()
+      : rawConfig);
+
+    return [configPath, userConfig] as const;
+  } else {
+    return [configPath, {} as UserConfig] as const;
+  }
+}
--- src/node/dev.ts
+++ src/node/dev.ts
@@ -2,8 +2,12 @@ import { createServer } from 'vite';
 import { pluginIndexHtml } from './plugins/indexHtml';
 import pluginReact from '@vitejs/plugin-react';
 import { PACKAGE_ROOT } from './constants';
+import { resolveConfig } from './config';
 
-export function createDevServer(root: string) {
+export async function createDevServer(root: string) {
+  // 获取 config 后面两个参数先 写死
+  const config = await resolveConfig(root, 'serve', 'development');
+  console.log('config', config);
   return createServer({
     root,
     plugins: [pluginIndexHtml(), pluginReact()],
