82a676f5692c77f8c6d70d77025153d814c8a189
wangmaoquan <1782976211@qq.com>
2023-05-26 14:10:22 +0800

    feat: 接入 remark-gfm

--- src/node/plugin-mdx/pluginMdxRollup.ts
+++ src/node/plugin-mdx/pluginMdxRollup.ts
@@ -1,8 +1,9 @@
 import pluginMdx from '@mdx-js/rollup';
+import remarkGFM from 'remark-gfm';
 
 export function pluginMdxRollup() {
   return pluginMdx({
-    remarkPlugins: [],
+    remarkPlugins: [remarkGFM],
     rehypePlugins: []
   });
 }
