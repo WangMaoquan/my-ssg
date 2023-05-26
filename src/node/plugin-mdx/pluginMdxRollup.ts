c1d3a362240e17406cc30aa707272836b1fad739
wangmaoquan <1782976211@qq.com>
2023-05-26 14:17:07 +0800

    feat: 接入rehype-autolink-headings, rehype-slug, 实现锚点

--- src/node/plugin-mdx/pluginMdxRollup.ts
+++ src/node/plugin-mdx/pluginMdxRollup.ts
@@ -1,9 +1,26 @@
 import pluginMdx from '@mdx-js/rollup';
 import remarkGFM from 'remark-gfm';
+// 下面两个插件一起使用 给 title 增加锚点
+import rehypePluginAutolinkHeadings from 'rehype-autolink-headings';
+import rehypePluginSlug from 'rehype-slug';
 
 export function pluginMdxRollup() {
   return pluginMdx({
     remarkPlugins: [remarkGFM],
-    rehypePlugins: []
+    rehypePlugins: [
+      rehypePluginSlug,
+      [
+        rehypePluginAutolinkHeadings,
+        {
+          properties: {
+            class: 'header-anchor'
+          },
+          content: {
+            type: 'text',
+            value: '#'
+          }
+        }
+      ]
+    ]
   });
 }
